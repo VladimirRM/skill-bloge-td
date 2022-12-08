4e7016fb7628a460e3996f0232506b58fe267c84
Vladimir RM <shvdrummer@gmail.com>
2022-12-08 10:23:15 +0200

    fin func createTodoForm

--- todo-app.js
+++ todo-app.js
@@ -1,9 +1,28 @@
 const createAppTitle = (title) => {
   const appTitle = document.createElement("h1");
   appTitle.innerHTML = title;
-  return appTitle
+  return appTitle;
 };
 
-const createTodoForm =()=>{
-    const form = document.createElement('form')
-}
+const createTodoForm = () => {
+  const form = document.createElement("form");
+  const input = document.createElement("input");
+  const addButton = document.createElement("button");
+  const wrapper = document.createElement("div");
+
+  form.classList.add("form-group", "mb-3");
+  input.classList.add("form-control");
+  input.placeholder = "Add Todo";
+  button.classList.add("btn", "btn-primary");
+  wrapper.classList.add("input-group-append");
+
+  wrapper.append(addButton);
+  form.append(input);
+  form.append(wrapper);
+
+  return {
+    form,
+    input,
+    addButton,
+  };
+};
