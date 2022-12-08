7e9acadd0a7283848d5ef7e304995cb2e915ea38
Vladimir RM <shvdrummer@gmail.com>
2022-12-08 10:10:21 +0200

    add const createTodoForm

--- todo-app.js
+++ todo-app.js
@@ -1,3 +1,9 @@
-const createAppTitle = (title)=>{
-   const appTitle = document.createElement('h1')
+const createAppTitle = (title) => {
+  const appTitle = document.createElement("h1");
+  appTitle.innerHTML = title;
+  return appTitle
+};
+
+const createTodoForm =()=>{
+    const form = document.createElement('form')
 }
